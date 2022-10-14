bfe2bb4c5b623c0e9b03b79adbfbc531e6976fab
leeyounhun <107522866+leeyounhun@users.noreply.github.com>
2022-10-14 09:30:40 +0900

    [level 1] Title: 폰켓몬, Time: 3.34 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        HashSet<Integer> set = new HashSet<>();
+        for(int n : nums)
+            set.add(n);
+        answer = (nums.length / 2 < set.size()) ? nums.length / 2 : set.size();
+        return answer;
+    }
+}
