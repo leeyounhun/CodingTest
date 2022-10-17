aed6a3323bdddec37130f17812400a9da29b5791
leeyounhun <107522866+leeyounhun@users.noreply.github.com>
2022-10-17 10:15:14 +0900

    [level 2] Title: 숫자의 표현, Time: 0.02 ms, Memory: 52.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int i = 1;
+        while (n > 0) {
+            if (n % i == 0)
+                answer++;
+            n -= i;
+            i++;
+        }
+        return answer;
+    }
+}
