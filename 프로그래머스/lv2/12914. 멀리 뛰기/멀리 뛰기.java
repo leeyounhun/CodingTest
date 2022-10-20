d64475b01a6db6b02b4c971629482feaf80e75bf
leeyounhun <107522866+leeyounhun@users.noreply.github.com>
2022-10-20 20:39:45 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.09 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        int[] arr = new int[2001];
+        arr[1] = 1;
+        arr[2] = 2;
+        for (int i = 3; i <= n; i++) 
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
+        return answer = arr[n];
+    }
+}
