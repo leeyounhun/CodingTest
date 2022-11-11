5c32d7ae96e0ab86c554f1007cd6d37051430827
leeyounhun <107522866+leeyounhun@users.noreply.github.com>
2022-11-11 16:26:57 +0900

    [level 2] Title: 프린터, Time: 0.75 ms, Memory: 65.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프린터/프린터.java
@@ -0,0 +1,23 @@
+import java.util.PriorityQueue;
+import java.util.Collections;
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int priority : priorities)
+            pq.offer(priority);
+        while(!pq.isEmpty()){
+            for (int i = 0; i < priorities.length; i++) {
+                if (priorities[i] == pq.peek()) {
+                    if (i == location) {
+                        answer++;
+                        return answer;
+                    }
+                    pq.poll();
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
